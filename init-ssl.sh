4cec464baa64a8234001ce28b9f929e1397c9532
Melissa Hale <melissakayh@gmail.com>
2023-09-25 12:55:07 -0500

    cert days controlled by env var and make port 5432 by default

--- init-ssl.sh
+++ init-ssl.sh
@@ -11,7 +11,7 @@ sudo chown postgres:postgres "$SSL_DIR"
 # Check if certificates already exist
 if [ ! -f "$SSL_DIR/server.key" ] || [ ! -f "$SSL_DIR/server.crt" ] || [ ! -f "$SSL_DIR/root.crt" ]; then
     # Generate Root CA
-    openssl req -new -x509 -days 820 -nodes -text -out "$SSL_DIR/root.crt" -keyout "$SSL_DIR/root.key" -subj "/CN=root-ca"
+    openssl req -new -x509 -days "${SSL_CERT_DAYS:-820}" -nodes -text -out "$SSL_DIR/root.crt" -keyout "$SSL_DIR/root.key" -subj "/CN=root-ca"
 
     # Generate Server Certificates
     openssl req -new -nodes -text -out "$SSL_DIR/server.csr" -keyout "$SSL_DIR/server.key" -subj "/CN=localhost"
